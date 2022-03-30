f9554925c0e06e98561b821410f8e9f9c5df6abc
Uuzics <87236102+Uuzics@users.noreply.github.com>
2022-03-30 21:00:51 +0800

    Init Maven project

---
+++ src/main/java/org/uuzics/simplecaptchasolverapi/SimpleCaptchaSolverApiApplication.java
@@ -0,0 +1,13 @@
+package org.uuzics.simplecaptchasolverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCaptchaSolverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCaptchaSolverApiApplication.class, args);
+	}
+
+}
